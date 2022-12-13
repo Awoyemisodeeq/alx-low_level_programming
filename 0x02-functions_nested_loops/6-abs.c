d0e36d9d0333ec4cb6c5bec2551a9a9e6bd50adc
awoyemisodeeq <awoyemisodeeq@gmail.com>
2022-12-13 13:32:00 -0800

    print absolute value function in c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,9 +1,12 @@
 #include "main.h"
+/**
+* _abs - returns absolute value of an int
+* @n: integer from which to get an absolute value
+* Return: Always 0
+*/
+int _abs(int n)
+{
 
-/**_abs - return absolute value of and integer
- * return: 0 always
- * 
- */
- int _abs(int n){
- return (n >= 0 ? n : n * -1);
- }
+return (n >= 0 ? n : n * -1);
+
+}
